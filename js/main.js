8dae0b527cd45d74d85340b98f9b8f9c222690a1
nguyen.thi.net <FRAMGIA\nguyen.thi.net@b121787.framgia.com>
2019-03-21 17:12:07 +0700

    update 21/3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,50 @@
 $(document).ready(function(){
-  var slideIndex = 0;
-  carousel();
+  // sidebar-collapse
 
- 
+  $('.js-btn-collaspe').on('click', function(){
+    $('.js-sidebar-collapse').slideToggle(300);
+  })
+
+  //Menu 
+  $('.js-menu-item').each(function () {
+
+    $(this).children('.js-menulink').on('click', function () {
+      var menuItem = $(this).parent('.js-menu-item');
+      if(menuItem.hasClass('main-menu__item--active')) {
+        menuItem.children('.js-submenu').slideUp(250);
+        menuItem.removeClass('main-menu__item--active');
+      }
+
+      else {
+        $('.main-menu > li').removeClass('main-menu__item--active');
+        $('.js-submenu').slideUp(300);
+        menuItem.children('.js-submenu').slideDown(250);
+        menuItem.addClass('main-menu__item--active');
+      }
+    })
+  })
+
+  $('.js-dropdown').on('click', function(){
+    $('.js-dropdown-menu').slideToggle(300);
+  })
+
+  //carousel 
+
+  $(".js-carousel-item:gt(0)").hide();
+
+  setInterval(function() {
+    $('.js-carousel-item:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('.js-carousel');
+  }, 3000);
+
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('.dropdown').length) {
+        $(".js-dropdown-menu").slideUp(200);
+    }
+  });
 })
 
-function carousel(){
-  var carouselItem = $('.js-carousel-item');
-  carouselItem.each(function(){
-    $(this).fadeOut();
-  })
-  slideIndex++;
-  if(slideIndex > carouselItem.lenght ){
-    slideIndex = 1;
-  }
-  
-}
